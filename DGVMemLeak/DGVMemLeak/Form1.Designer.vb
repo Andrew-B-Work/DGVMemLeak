e60317687a0e32a7aca28d6d7dcd8101126a4584
Andrew-B-Work <51759879+Andrew-B-Work@users.noreply.github.com>
2019-06-12 14:17:21 -0400

    Add files via upload
    The project

---
+++ DGVMemLeak/DGVMemLeak/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DGV1 = New System.Windows.Forms.DataGridView()
+        Me.ColA = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColB = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.ColC = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.ColD = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.BTN_Fill = New System.Windows.Forms.Button()
+        Me.btn_Simple_Clear = New System.Windows.Forms.Button()
+        Me.BTN_Custom_Clear = New System.Windows.Forms.Button()
+        Me.RB_MainThread = New System.Windows.Forms.RadioButton()
+        Me.RB_ThreadPool = New System.Windows.Forms.RadioButton()
+        Me.NUD_Threads = New System.Windows.Forms.NumericUpDown()
+        Me.NUD_Rows = New System.Windows.Forms.NumericUpDown()
+        Me.BTN_GC = New System.Windows.Forms.Button()
+        Me.BTN_Report = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TB_Console = New System.Windows.Forms.TextBox()
+        Me.BTN_KWClear = New System.Windows.Forms.Button()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_Threads, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUD_Rows, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DGV1
+        '
+        Me.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColA, Me.ColB, Me.ColC, Me.ColD})
+        Me.DGV1.Location = New System.Drawing.Point(0, 0)
+        Me.DGV1.Name = "DGV1"
+        Me.DGV1.Size = New System.Drawing.Size(439, 353)
+        Me.DGV1.TabIndex = 0
+        '
+        'ColA
+        '
+        Me.ColA.HeaderText = "ColA"
+        Me.ColA.Name = "ColA"
+        '
+        'ColB
+        '
+        Me.ColB.HeaderText = "ColB"
+        Me.ColB.Name = "ColB"
+        '
+        'ColC
+        '
+        Me.ColC.HeaderText = "ColC"
+        Me.ColC.Name = "ColC"
+        '
+        'ColD
+        '
+        Me.ColD.HeaderText = "ColD"
+        Me.ColD.Name = "ColD"
+        '
+        'BTN_Fill
+        '
+        Me.BTN_Fill.Location = New System.Drawing.Point(0, 359)
+        Me.BTN_Fill.Name = "BTN_Fill"
+        Me.BTN_Fill.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_Fill.TabIndex = 1
+        Me.BTN_Fill.Text = "Fill"
+        Me.BTN_Fill.UseVisualStyleBackColor = True
+        '
+        'btn_Simple_Clear
+        '
+        Me.btn_Simple_Clear.Location = New System.Drawing.Point(0, 388)
+        Me.btn_Simple_Clear.Name = "btn_Simple_Clear"
+        Me.btn_Simple_Clear.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Simple_Clear.TabIndex = 2
+        Me.btn_Simple_Clear.Text = "Simple Clear"
+        Me.btn_Simple_Clear.UseVisualStyleBackColor = True
+        '
+        'BTN_Custom_Clear
+        '
+        Me.BTN_Custom_Clear.Location = New System.Drawing.Point(0, 417)
+        Me.BTN_Custom_Clear.Name = "BTN_Custom_Clear"
+        Me.BTN_Custom_Clear.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_Custom_Clear.TabIndex = 3
+        Me.BTN_Custom_Clear.Text = "Custom Clear"
+        Me.BTN_Custom_Clear.UseVisualStyleBackColor = True
+        '
+        'RB_MainThread
+        '
+        Me.RB_MainThread.AutoSize = True
+        Me.RB_MainThread.Checked = True
+        Me.RB_MainThread.Location = New System.Drawing.Point(82, 422)
+        Me.RB_MainThread.Name = "RB_MainThread"
+        Me.RB_MainThread.Size = New System.Drawing.Size(85, 17)
+        Me.RB_MainThread.TabIndex = 4
+        Me.RB_MainThread.TabStop = True
+        Me.RB_MainThread.Text = "Main Thread"
+        Me.RB_MainThread.UseVisualStyleBackColor = True
+        '
+        'RB_ThreadPool
+        '
+        Me.RB_ThreadPool.AutoSize = True
+        Me.RB_ThreadPool.Location = New System.Drawing.Point(183, 422)
+        Me.RB_ThreadPool.Name = "RB_ThreadPool"
+        Me.RB_ThreadPool.Size = New System.Drawing.Size(80, 17)
+        Me.RB_ThreadPool.TabIndex = 5
+        Me.RB_ThreadPool.TabStop = True
+        Me.RB_ThreadPool.Text = "ThreadPool"
+        Me.RB_ThreadPool.UseVisualStyleBackColor = True
+        '
+        'NUD_Threads
+        '
+        Me.NUD_Threads.Location = New System.Drawing.Point(332, 422)
+        Me.NUD_Threads.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.NUD_Threads.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NUD_Threads.Name = "NUD_Threads"
+        Me.NUD_Threads.Size = New System.Drawing.Size(120, 20)
+        Me.NUD_Threads.TabIndex = 6
+        Me.NUD_Threads.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NUD_Rows
+        '
+        Me.NUD_Rows.Location = New System.Drawing.Point(163, 362)
+        Me.NUD_Rows.Maximum = New Decimal(New Integer() {100000, 0, 0, 0})
+        Me.NUD_Rows.Minimum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NUD_Rows.Name = "NUD_Rows"
+        Me.NUD_Rows.Size = New System.Drawing.Size(120, 20)
+        Me.NUD_Rows.TabIndex = 7
+        Me.NUD_Rows.Value = New Decimal(New Integer() {1000, 0, 0, 0})
+        '
+        'BTN_GC
+        '
+        Me.BTN_GC.Location = New System.Drawing.Point(377, 364)
+        Me.BTN_GC.Name = "BTN_GC"
+        Me.BTN_GC.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_GC.TabIndex = 8
+        Me.BTN_GC.Text = "GC"
+        Me.BTN_GC.UseVisualStyleBackColor = True
+        '
+        'BTN_Report
+        '
+        Me.BTN_Report.Location = New System.Drawing.Point(377, 393)
+        Me.BTN_Report.Name = "BTN_Report"
+        Me.BTN_Report.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_Report.TabIndex = 9
+        Me.BTN_Report.Text = "report"
+        Me.BTN_Report.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 364)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Rows"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(278, 424)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Chunks"
+        '
+        'TB_Console
+        '
+        Me.TB_Console.BackColor = System.Drawing.Color.Black
+        Me.TB_Console.Dock = System.Windows.Forms.DockStyle.Right
+        Me.TB_Console.ForeColor = System.Drawing.Color.Lime
+        Me.TB_Console.Location = New System.Drawing.Point(458, 0)
+        Me.TB_Console.Multiline = True
+        Me.TB_Console.Name = "TB_Console"
+        Me.TB_Console.Size = New System.Drawing.Size(342, 450)
+        Me.TB_Console.TabIndex = 12
+        '
+        'BTN_KWClear
+        '
+        Me.BTN_KWClear.Location = New System.Drawing.Point(81, 388)
+        Me.BTN_KWClear.Name = "BTN_KWClear"
+        Me.BTN_KWClear.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_KWClear.TabIndex = 13
+        Me.BTN_KWClear.Text = "Kyle Clear"
+        Me.BTN_KWClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BTN_KWClear)
+        Me.Controls.Add(Me.TB_Console)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BTN_Report)
+        Me.Controls.Add(Me.BTN_GC)
+        Me.Controls.Add(Me.NUD_Rows)
+        Me.Controls.Add(Me.NUD_Threads)
+        Me.Controls.Add(Me.RB_ThreadPool)
+        Me.Controls.Add(Me.RB_MainThread)
+        Me.Controls.Add(Me.BTN_Custom_Clear)
+        Me.Controls.Add(Me.btn_Simple_Clear)
+        Me.Controls.Add(Me.BTN_Fill)
+        Me.Controls.Add(Me.DGV1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_Threads, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUD_Rows, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DGV1 As DataGridView
+    Friend WithEvents ColA As DataGridViewTextBoxColumn
+    Friend WithEvents ColB As DataGridViewComboBoxColumn
+    Friend WithEvents ColC As DataGridViewComboBoxColumn
+    Friend WithEvents ColD As DataGridViewComboBoxColumn
+    Friend WithEvents BTN_Fill As Button
+    Friend WithEvents btn_Simple_Clear As Button
+    Friend WithEvents BTN_Custom_Clear As Button
+    Friend WithEvents RB_MainThread As RadioButton
+    Friend WithEvents RB_ThreadPool As RadioButton
+    Friend WithEvents NUD_Threads As NumericUpDown
+    Friend WithEvents NUD_Rows As NumericUpDown
+    Friend WithEvents BTN_GC As Button
+    Friend WithEvents BTN_Report As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TB_Console As TextBox
+    Friend WithEvents BTN_KWClear As Button
+End Class
---
+++ DGVMemLeak/DGVMemLeak/Form1.vb
@@ -0,0 +1,349 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Dim dtMain As DataTable = New DataTable("dtMain")
+    Dim dtB As DataTable = New DataTable("dtB")
+    Dim dtC As DataTable = New DataTable("dtC")
+    Dim dtD As DataTable = New DataTable("dtD")
+
+    Dim tableDict As Dictionary(Of String, DataTable) = New Dictionary(Of String, DataTable)()
+
+    Dim r As Random = New Random()
+
+    Dim weaks As WeakReference()
+
+    Dim threadCount As Integer = 0
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        tableDict.Add(dtMain.TableName, dtMain)
+        tableDict.Add(dtB.TableName, dtB)
+        tableDict.Add(dtC.TableName, dtC)
+        tableDict.Add(dtD.TableName, dtD)
+
+        tableDict.Add("ColB", dtB)
+        tableDict.Add("ColC", dtC)
+        tableDict.Add("ColD", dtD)
+
+        'DT main data columns
+        Dim c As DataColumn = New DataColumn()
+        c.ColumnName = "colA"
+        c.DataType = GetType(String)
+        dtMain.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "ColB"
+        c.DataType = GetType(Integer)
+        dtMain.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "ColC"
+        c.DataType = GetType(Integer)
+        dtMain.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "ColD"
+        c.DataType = GetType(Integer)
+        dtMain.Columns.Add(c)
+
+
+        c = New DataColumn()
+        c.ColumnName = "colE"
+        c.DataType = GetType(Long)
+        dtMain.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colF"
+        c.DataType = GetType(Double)
+        dtMain.Columns.Add(c)
+
+        'dtB
+        c = New DataColumn()
+        c.ColumnName = "val"
+        c.DataType = GetType(Integer)
+        dtB.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "display"
+        c.DataType = GetType(String)
+        dtB.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colE"
+        c.DataType = GetType(Long)
+        dtB.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colF"
+        c.DataType = GetType(Double)
+        dtB.Columns.Add(c)
+
+        'dtC
+        c = New DataColumn()
+        c.ColumnName = "val"
+        c.DataType = GetType(Integer)
+        dtC.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "display"
+        c.DataType = GetType(String)
+        dtC.Columns.Add(c)
+
+        'dtD
+        c = New DataColumn()
+        c.ColumnName = "val"
+        c.DataType = GetType(Integer)
+        dtD.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "display"
+        c.DataType = GetType(String)
+        dtD.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colE"
+        c.DataType = GetType(Long)
+        dtD.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colF"
+        c.DataType = GetType(Double)
+        dtD.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colG"
+        c.DataType = GetType(Long)
+        dtD.Columns.Add(c)
+
+        c = New DataColumn()
+        c.ColumnName = "colH"
+        c.DataType = GetType(Double)
+        dtD.Columns.Add(c)
+
+        addRows(dtB, r.Next(15, 30))
+
+        addRows(dtC, r.Next(150, 300))
+
+        addRows(dtD, r.Next(1500, 3000))
+
+        'Setup dgv
+        For Each col As DataGridViewColumn In DGV1.Columns
+            If TypeOf (col) Is DataGridViewComboBoxColumn Then
+                Dim cmb As DataGridViewComboBoxColumn = CType(col, DataGridViewComboBoxColumn)
+                cmb.DataSource = tableDict(cmb.Name)
+                cmb.DataPropertyName = "val"
+                cmb.ValueMember = "val"
+                cmb.DisplayMember = "display"
+            End If
+        Next
+
+    End Sub
+    Private Sub BTN_Fill_Click(sender As Object, e As EventArgs) Handles BTN_Fill.Click
+        If DGV1.Rows.Count > 1 Then
+            consoleOut("DGV Has rows, can't fill")
+            Return
+        End If
+        consoleOut("Ensuring min rows")
+        If dtMain.Rows.Count < CInt(NUD_Rows.Value) Then
+            addRows(dtMain, CInt(NUD_Rows.Value) - dtMain.Rows.Count)
+        End If
+
+        consoleOut("Filling DGV")
+        DGV1.RowCount = CInt(NUD_Rows.Value)
+        weaks = New WeakReference(CInt(NUD_Rows.Value) - 1) {}
+
+        For i As Integer = 0 To CInt(NUD_Rows.Value) - 1
+            Dim dgvr As DataGridViewRow = DGV1.Rows(i)
+            Dim dr As DataRow = dtMain.Rows(i)
+            weaks(i) = New WeakReference(DGV1.Rows(i))
+            For Each col As DataGridViewColumn In DGV1.Columns
+                dgvr.Cells(col.Name).Value = dr(col.Name)
+            Next
+        Next
+
+        consoleOut("Filled")
+
+    End Sub
+
+    Private Sub consoleOut(v As String)
+        TB_Console.AppendText(v + vbNewLine)
+    End Sub
+
+    Private Sub addRows(dt As DataTable, cnt As Integer)
+        Dim s As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ .,()-+=?!@#abcdefghijklmnopqrstuvwxyz0123456789"
+
+        Dim lastIndex As Integer = -1
+        If dt IsNot dtMain Then
+            If dt.Rows.Count = 0 Then
+                lastIndex = 0
+            Else
+                lastIndex = dt.Rows(dt.Rows.Count - 1)("val")
+            End If
+        End If
+
+        For i As Integer = 0 To cnt - 1
+            If r.Next(15) = 10 Then
+                lastIndex += r.Next(2, 99)
+            Else
+                lastIndex += 1
+            End If
+
+
+            Dim dr As DataRow = dt.NewRow()
+            For Each c As DataColumn In dt.Columns
+                If c.DataType = GetType(String) Then
+                    Dim sLen As Integer = r.Next(5, 100)
+                    Dim sRes As String = ""
+                    For si As Integer = 0 To sLen - 1
+                        sRes += s.Chars(r.Next(0, s.Length))
+                    Next
+                    dr(c.ColumnName) = sRes
+
+                ElseIf c.DataType = GetType(Long) Then
+                    dr(c.ColumnName) = r.Next()
+
+                ElseIf c.DataType = GetType(Double) Then
+                    dr(c.ColumnName) = r.NextDouble()
+
+                ElseIf c.DataType = GetType(Integer) Then
+                    If dt IsNot dtMain Then
+                        dr(c.ColumnName) = lastIndex
+                    Else
+                        Dim parTable As DataTable = tableDict(c.ColumnName)
+                        dr(c.ColumnName) = parTable(r.Next(parTable.Rows.Count))("val")
+                    End If
+                End If
+
+            Next
+            dt.Rows.Add(dr)
+        Next
+
+
+    End Sub
+
+    Private Sub Btn_Simple_Clear_Click(sender As Object, e As EventArgs) Handles btn_Simple_Clear.Click
+
+        consoleOut("Clearing rows")
+        DGV1.Rows.Clear()
+        consoleOut("Done Clearing rows")
+    End Sub
+
+    Private Sub BTN_Report_Click(sender As Object, e As EventArgs) Handles BTN_Report.Click
+        Dim livingRows As Integer = 0
+        For i As Integer = 0 To weaks.Length - 1
+            If weaks(i).IsAlive Then
+                livingRows += 1
+            End If
+        Next
+        consoleOut(String.Format("GC bytes {0}, living rows {1} ", GC.GetTotalMemory(True), livingRows))
+    End Sub
+
+    Private Sub BTN_GC_Click(sender As Object, e As EventArgs) Handles BTN_GC.Click
+        consoleOut("GC")
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+        GC.Collect()
+        consoleOut("Done GC")
+    End Sub
+
+    Private Sub BTN_Custom_Clear_Click(sender As Object, e As EventArgs) Handles BTN_Custom_Clear.Click
+        Dim dt As DateTime = DateTime.Now()
+        Dim ts As TimeSpan
+        'Get all combo columns
+        Dim cols As List(Of Integer) = New List(Of Integer)
+        For Each c As DataGridViewColumn In DGV1.Columns
+            If TypeOf (c) Is DataGridViewComboBoxColumn Then
+                cols.Add(c.Index)
+            End If
+        Next
+
+        Dim rows As DataGridViewRow() = New DataGridViewRow(DGV1.RowCount - 1) {}
+        DGV1.Rows.CopyTo(rows, 0)
+
+        If RB_MainThread.Checked Then
+            consoleOut("Doing this on main!")
+            DGV1.Rows.Clear()
+            Interlocked.Increment(threadCount)
+            Dim tdp = New TearDownParms(rows, cols, 0, rows.Count - 1)
+            Teardown_DGV_Rows(tdp)
+            ts = DateTime.Now - dt
+            consoleOut("Finished in " + ts.ToString())
+        Else
+            Dim chunks As Integer = CInt(NUD_Threads.Value)
+            consoleOut("Doing this on threads in " + chunks.ToString() + " chunks")
+            DGV1.Rows.Clear()
+            Dim size As Integer = rows.Count \ chunks
+            Dim max As Integer = 0
+            For i As Integer = 0 To chunks - 2
+                Interlocked.Increment(threadCount)
+                max = (i + 1) * size - 1
+                ThreadPool.QueueUserWorkItem(AddressOf Teardown_DGV_Rows, New TearDownParms(rows, cols, i * size, max))
+            Next
+            Interlocked.Increment(threadCount)
+            ThreadPool.QueueUserWorkItem(AddressOf Teardown_DGV_Rows, New TearDownParms(rows, cols, max, rows.Count - 1))
+        End If
+
+        'now we wait
+        While threadCount <> 0
+            'with the amount of time taken here 1s is not going to ruin my timing
+            Thread.Sleep(1000)
+        End While
+        ts = DateTime.Now - dt
+        consoleOut("Finished in " + ts.ToString())
+    End Sub
+
+    Private Sub Teardown_DGV_Rows(state As Object)
+        Dim tdp As TearDownParms = CType(state, TearDownParms)
+        For i As Integer = tdp.startNDX To tdp.stopNDX
+            If tdp.rows(i) IsNot Nothing Then
+                For Each c As Integer In tdp.colNDXS
+
+                    Dim cell As DataGridViewComboBoxCell = tdp.rows(i).Cells(c)
+                    cell.DataSource = Nothing
+                    cell = Nothing
+                Next
+                tdp.rows(i) = Nothing
+            End If
+        Next
+        Interlocked.Decrement(threadCount)
+    End Sub
+
+    Private Sub BTN_KWClear_Click(sender As Object, e As EventArgs) Handles BTN_KWClear.Click
+        consoleOut("Doing this Kyle's way!")
+        Dim dt As DateTime = DateTime.Now()
+        DGV1.SuspendLayout()
+        For i As Integer = 0 To DGV1.Rows.Count - 1
+            For Each c As DataGridViewColumn In DGV1.Columns
+                If TypeOf (c) Is DataGridViewComboBoxColumn Then
+                    CType(DGV1(c.Index, i), DataGridViewComboBoxCell).DataSource = Nothing
+                End If
+            Next
+        Next
+        DGV1.Rows.Clear()
+        DGV1.ResumeLayout()
+
+        Dim ts As TimeSpan = DateTime.Now - dt
+        consoleOut("Finished in " + ts.ToString())
+    End Sub
+End Class
+
+Class TearDownParms
+    Public startNDX As Integer
+    Public stopNDX As Integer
+    Public rows As DataGridViewRow()
+    Public colNDXS As List(Of Integer)
+
+
+
+    Public Sub New(rows() As DataGridViewRow, cols As List(Of Integer), start As Integer, [stop] As Integer)
+        Me.rows = rows
+        Me.colNDXS = cols
+        Me.startNDX = start
+        Me.stopNDX = [stop]
+    End Sub
+End Class
